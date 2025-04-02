a176b3385ec420b326f7f1565acb91c81e89303d
XunFlowerrr <105523593+XunFlowerrr@users.noreply.github.com>
2025-04-02 19:52:48 +0700

    7.2.5 ส่งข้อมูลไปยัง Backend

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,13 @@
 // authController.js
 export const registerUser = async (req, res) => {
-  res.status(200).send({ message: "User registered successfully" });
+  const { username, password } = req.body; // รับข้อมูลจาก Body
+  res
+    .status(200)
+    .send({ message: "User registered successfully", username, password });
 };
 export const loginUser = async (req, res) => {
-  res.status(200).send({ message: "User logged in successfully" });
+  const { username, password } = req.body; // รับข้อมูลจาก Body
+  res
+    .status(200)
+    .send({ message: "User logged in successfully", username, password });
 };
